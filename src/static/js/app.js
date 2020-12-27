c845b0d103b602181edb396bf7ed0a0f67404d27
Shaira Osmani <shairashegarf@gmail.com>
2020-12-27 20:30:48 +0900

    Header added.

--- src/static/js/app.js
+++ src/static/js/app.js
@@ -4,7 +4,8 @@ function App() {
         <Container>
             <Row>
                 <Col md={{ offset: 3, span: 6 }}>
-                
+                <h1>--- Todo App ---</h1>
+                <TodoListCard />
                 </Col>
             </Row>
         </Container>
